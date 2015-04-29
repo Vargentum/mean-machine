59ed167830618e6d32b20d630136068ce9372f76
Vargentum <vargentum@gmail.com>
2015-04-29 18:58:37 +0300

    express hello world

--- awesome-test/server.js
+++ awesome-test/server.js
@@ -1,17 +1,10 @@
-var http = require('http'),
-    fs = require('fs')
+var express = require('express');
+var app = express();
+var path = require('path');
 
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'))
+});
 
-http.createServer(function(req, res) {
-
-  res.writeHead(200, {
-    'Content-Type': 'text/html',
-    'Access-Control-Allow-Origin': '*'
-  });
-
-  var readStream = fs.createReadStream(__dirname + '/index.html');
-  readStream.pipe(res);
-
-}).listen(1337)
-
-console.log('Visit me at http://localhost:1337')
+app.listen(1337);
+console.log('1337 is the magic port');
